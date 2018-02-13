adfc85680f93858a0abb465a4767d7c5b7d5db52
kreulenk <kreulenk@gmail.com>
2018-02-12 23:28:35 -0500

    Actually removed bug this time. Working list of location objects now loadable from XML file.

--- GTNav/GTNavPage.xaml.cs
+++ GTNav/GTNavPage.xaml.cs
@@ -14,31 +14,30 @@ namespace GTNav
 {
     public partial class GTNavPage : ContentPage
     {
-        private List<Location> locationList = null;
         public GTNavPage()
         {
             InitializeComponent();
-            LoadXMLData();
+            List<Location> tempList = LoadXMLData();
+            Debug.WriteLine(tempList.First());
         }
 
 
-        private async void LoadXMLData()
+        private List<Location> LoadXMLData()
         {
             string resPrefix = "GTNav.";
-
             var assembly = typeof(GTNavPage).GetTypeInfo().Assembly;
+            IEnumerable<Location> locations = null;
             Stream stream = assembly.GetManifestResourceStream(resPrefix + "Locations.xml");
-            await Task.Factory.StartNew(delegate {
-                XDocument doc = XDocument.Load(stream);
-                IEnumerable<Location> locations = from l in doc.Descendants("Location")
-                                          select new Location
-                                          {
-                                              Name = l.Attribute("Name").Value,
-                                              Latititude = float.Parse(l.Attribute("Latitude").Value),
-                                              Longitude = float.Parse(l.Attribute("Longitude").Value)
-                                          };
-                locationList = locations.ToList();
-            });
+            XDocument doc = XDocument.Load(stream);
+            locations = from l in doc.Descendants("Location")
+                  select new Location
+                  {
+                      Name = l.Attribute("Name").Value,
+                      Latititude = float.Parse(l.Attribute("Latitude").Value),
+                      Longitude = float.Parse(l.Attribute("Longitude").Value)
+                  };
+            return locations.ToList();
+
         }
     }
 }
