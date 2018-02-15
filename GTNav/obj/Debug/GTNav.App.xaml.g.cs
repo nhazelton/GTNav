24c1e719f26000e6be1b361e5a47c9c13360c736
nhazelton6 <nhazelton6@gatech.edu>
2018-02-15 11:53:40 -0500

    Added search bar -- one file couldn't be staged so watch out

--- GTNav/obj/Debug/GTNav.App.xaml.g.cs
+++ GTNav/obj/Debug/GTNav.App.xaml.g.cs
@@ -11,7 +11,7 @@
 namespace GTNav {
     
     
-    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("/Users/kevin/Projects/GTNav/GTNav/App.xaml")]
+    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("C:\\Users\\Nate\\Documents\\GitHub\\GTNav\\GTNav\\App.xaml")]
     public partial class App : global::Xamarin.Forms.Application {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
