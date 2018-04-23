6ca2803c13290af3742eeefc9b68c47f65af057c
nhazelton6 <nhazelton6@gatech.edu>
2018-04-23 03:12:11 -0400

    Map instance live on Red page

--- GTNav/RedRoutePage.xaml.cs
+++ GTNav/RedRoutePage.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace GTNav
@@ -12,9 +13,22 @@ namespace GTNav
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class RedRoutePage : ContentPage
 	{
-		public RedRoutePage ()
+
+        CampusMap campusMap;
+
+		public RedRoutePage()
 		{
-			InitializeComponent ();
-		}
+			InitializeComponent();
+
+            campusMap = MyRedMap;
+            var sampleMarker = new Position(33.774671, -84.396374);
+            campusMap.Marker = new BusMarker
+            {
+                Position = sampleMarker,
+                Radius = 100
+            };
+
+            campusMap.MoveToRegion(MapSpan.FromCenterAndRadius(sampleMarker, Distance.FromMiles(0.6)));
+        }
 	}
 }
